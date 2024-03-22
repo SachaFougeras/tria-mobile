8b824e109b40bf0bd31934fe1c69bb6fb33583af
Sacha Fougeras <fougerassacha@gmail.com>
2024-03-22 23:14:17 +0100

    update menu

--- composants/MyTabs.js
+++ composants/MyTabs.js
@@ -1,30 +1,33 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeIcon from '../composants/atoms/icons/HomeIcon';
 import SpectacleIcon from './atoms/icons/SpectaclesIcon';
 import TheatreIcon from './atoms/icons/MasquesTheatre';
 import LoginIcon from './atoms/icons/LoginIcon';
 import HomeScreen from '../composants/home';
+import AccountScreen from '../composants/compte';
 import React from 'react';
 
 
 function SettingsScreen({ navigation }) {
   React.useLayoutEffect(() => {
     navigation.setOptions({
-      
+      headerStyle: {
+        backgroundColor: 'black',
+      },
+      headerTintColor: 'white',
       headerLeft: () => (
-        
         <Image
           source={require('../assets/image.png')}
-          style={{ width: 50, height: 50, marginLeft: 10 }}
+          style={styles.logo}
         />
       ),
     });
   }, [navigation]);
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text></Text>
     </View>
   );
 }
@@ -59,18 +62,30 @@ screenOptions={{
   />
   <Tab.Screen
     name="Théâtres"
-    component={HomeScreen}
+    component={SettingsScreen}
     options={{
       tabBarIcon: ({ color, size }) => <TheatreIcon color={color} size={size} />,
     }}
   />
   <Tab.Screen
     name="Compte"
-    component={SettingsScreen}
+    component={AccountScreen}
     options={{
       tabBarIcon: ({ color, size }) => <LoginIcon color={color} size={size} />,
     }}
   />
 </Tab.Navigator>
   );
+  
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#140A0A',
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    marginLeft: 10,
+  }
+});
